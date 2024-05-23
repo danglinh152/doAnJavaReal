b93f80fbebd5beb508f3836fc7d81199a06fd1e6
danglinh152 <danglinh.k4@gmail.com>
2024-05-23 15:49:13 +0700

    fix the database

--- src/database/QLNVdb.sql
+++ src/database/QLNVdb.sql
@@ -20,7 +20,7 @@ drop table yeucau
 /
 drop table nhanvien
 /
-ALTER SESSION set NLS_DATE_FORMAT = 'DD-MM-YYYY';
+ALTER SESSION set NLS_DATE_FORMAT = 'YYYY-MM-DD';
 set feedback ON
 set serveroutput on;
 
@@ -115,6 +115,20 @@ constraint chamcong_sogiotangca_ck check (sogiotangca > 0 and sogiotangca <= 48)
 );
 /
 
+CREATE TABLE YEUCAU (
+mayc number(4),
+manv number(4),
+noidung nvarchar2(500),
+trangthai number(1),
+constraint yeucau_pk primary key (mayc),
+constraint yeucau_fk foreign key (manv) references NHANVIEN (manv),
+constraint yeucau_ck check (trangthai IN (0,1))
+);
+
+--Insert
+INSERT INTO PHONGBAN VALUES (1, 'IT', '2024-05-23', null, null);
+INSERT INTO NHANVIEN VALUES (1, 'Đặng Quang Khánh Linh', 'Nam', '2004-02-15', '0355662648', '22520756@gm.uit.edu.vn', '1401/4, tổ 17, ấp Phước Hòa, xã Long Phước, huyện Long Thành, tỉnh Đồng Nai', '075204005822', 'Fresher', 1);
+INSERT INTO TAIKHOAN VALUES (1, 1, 'admin', 'admin', 'quản lý');
 
 
 
@@ -125,7 +139,6 @@ constraint chamcong_sogiotangca_ck check (sogiotangca > 0 and sogiotangca <= 48)
 
 
 
---Trigger Ng� y nh?n ch?c c?a tr??ng phòng ph?i l?n h?n ng� y sinh c?a ng??i ?ó
 CREATE OR REPLACE TRIGGER TRG_NGNHANCHUC_NGSINH
 BEFORE INSERT OR UPDATE ON PHONGBAN
 FOR EACH ROW
@@ -142,18 +155,21 @@ BEGIN
     ELSE DBMS_OUTPUT.PUT_LINE('Them/sua du lieu thanh cong');
     END IF;
 END;
+/
 
---Trigger Ng� y nh?n ch?c c?a tr??ng phòng ph?i l?n h?n ho?c b?ng ng� y th� nh l?p c?a phòng ban ?ó
+
+
 CREATE OR REPLACE TRIGGER TRG_NGNHANCHUC_NGTHANHLAP
 BEFORE INSERT OR UPDATE ON PHONGBAN
 FOR EACH ROW
 BEGIN
-    IF :NEW.NGAYNHANCHUC < :NEW.NGAYTHANHLAP THEN
+    IF :NEW.NGAYNHANCHUC < :NEW.NGTHANHLAP THEN
         RAISE_APPLICATION_ERROR(-20001, 'Ngay nhan chuc phai lon hon hoac bang ngay thanh lap cua phong ban');
     ELSE 
         DBMS_OUTPUT.PUT_LINE('Thêm/s?a d? li?u th� nh công');
     END IF;
 END;
+/
 
 --Trigger M?i nhân viên không có quá 10 k? n?ng
 CREATE OR REPLACE TRIGGER TRG_NV_KN
@@ -171,21 +187,7 @@ BEGIN
     ELSE DBMS_OUTPUT.PUT_LINE('Thêm/s?a d? li?u th� nh công');
     END IF;
 END;
-
-
---NGÀY B?T ??U LÀM VI?C C?A 1 NH N VIÊN PH?I L?N H?N NGÀY SINH
-CREATE OR REPLACE TRIGGER TG_1 
-BEFORE INSERT OR UPDATE ON NHANVIEN
-    FOR EACH ROW
-        BEGIN        
-             IF(:NEW.NGAYBD < :NEW.NGSINH) THEN
-                     RAISE_APPLICATION_ERROR(-20001, 'L?i');
-                ELSE
-                    DBMS_OUTPUT.PUT_LINE('THANH CONG!');
-                END IF;
-        END;     
-
-
+/
 --S? ng� y ?i l� m, ?i tr?, v?ng không ???c quá
 -- N?u tháng có 30 ng� y: không ???c quá 30 ng� y
 -- N?u tháng có 31 ng� y: không ???c quá 31 ng� y
@@ -194,17 +196,17 @@ CREATE OR REPLACE TRIGGER TG_2
 BEFORE INSERT OR UPDATE ON CHAMCONG
     FOR EACH ROW
     BEGIN
-        IF(:NEW.THANGLAMVIEC IN (4, 6, 9, 11) AND (:NEW.SONGAYLV > 30 OR :NEW.SONGAYDITRE > 30 OR :NEW.SONGAYNGHI > 30)) THEN
+        IF(:NEW.THANGLAMVIEC IN (4, 6, 9, 11) AND (:NEW.SONGAYLAMVIEC > 30 OR :NEW.SONGAYDITRE > 30 OR :NEW.SONGAYNGHI > 30)) THEN
              RAISE_APPLICATION_ERROR(-20001, 'L?i');
-        ELSIF(:NEW.THANGLAMVIEC IN (1, 3, 5, 7, 8, 10, 12) AND (:NEW.SONGAYLV > 31 OR :NEW.SONGAYDITRE > 31 OR :NEW.SONGAYNGHI > 31)) THEN
+        ELSIF(:NEW.THANGLAMVIEC IN (1, 3, 5, 7, 8, 10, 12) AND (:NEW.SONGAYLAMVIEC > 31 OR :NEW.SONGAYDITRE > 31 OR :NEW.SONGAYNGHI > 31)) THEN
              RAISE_APPLICATION_ERROR(-20001, 'L?i');
-        ELSIF(:NEW.THANGLAMVIEC = 2 AND (:NEW.SONGAYLV > 29 OR :NEW.SONGAYDITRE > 29 OR :NEW.SONGAYNGHI > 29)) THEN
+        ELSIF(:NEW.THANGLAMVIEC = 2 AND (:NEW.SONGAYLAMVIEC > 29 OR :NEW.SONGAYDITRE > 29 OR :NEW.SONGAYNGHI > 29)) THEN
              RAISE_APPLICATION_ERROR(-20001, 'L?i');
         ELSE
             DBMS_OUTPUT.PUT_LINE('THANH CONG');
         END IF;
     END;   
-    
+/
     
 --Thu?c tính NGSINH c?a NHANVIEN ph?i nh? h?n ho?c b?ng ng� y hi?n t?i
 CREATE OR REPLACE TRIGGER TG_3
@@ -217,7 +219,7 @@ BEFORE INSERT OR UPDATE ON NHANVIEN
                 DBMS_OUTPUT.PUT_LINE('THANHCONG');
             END IF;
         END;
-            
+/            
             
         
 --M?i tr??ng phòng ch? l� m tr??ng phòng duy nh?t 1 phòng ban
@@ -237,7 +239,7 @@ BEFORE INSERT OR UPDATE ON PHONGBAN
                 DBMS_OUTPUT.PUT_LINE('THANHCONG');
             END IF;
         END;
-
+/
 
 --trigger m?i phòng ban có t?i ?a 200 nhân viên
 CREATE OR REPLACE TRIGGER TRG_NV_TOIDA
@@ -254,7 +256,7 @@ BEGIN
         RAISE_APPLICATION_ERROR(-20001, 'Phòng ban nay da dat so luong toi da.');
     END IF;
 END;
-
+/
 
 ----trigger tr??ng phòng ph?i có kinh nghi?m trên 6 n?m
 --CREATE OR REPLACE TRIGGER CHECK_TRUONGPHONG
@@ -279,19 +281,6 @@ END;
 --END;
 
 
---trigger nhân viên v� o l� m ph?i t? ?? 18 tu?i
-CREATE OR REPLACE TRIGGER CHECK_NHANVIEN_AGE
-BEFORE INSERT OR UPDATE ON NHANVIEN
-FOR EACH ROW
-DECLARE
-    v_tuoi NUMBER;
-BEGIN
-    -- Tính tu?i c?a nhân viên m?i
-    v_tuoi := EXTRACT(YEAR FROM :NEW.NGAYBD) - EXTRACT(YEAR FROM :NEW.NGSINH);
 
-    -- Ki?m tra n?u tu?i c?a nhân viên m?i d??i 18
-    IF v_tuoi < 18 THEN
-        RAISE_APPLICATION_ERROR(-20003, 'Nhân viên ph?i t? ?? 18 tu?i ?? v� o l� m.');
-    END IF;
-END;
+
 
